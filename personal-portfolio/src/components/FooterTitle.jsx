9917ae6a193a054d5ccbd4009ef05d65b561e53d
salmamoussaa25 <salmamoussaa25@gmail.com>
2025-12-20 17:03:05 +0200

    feat: added Footer title component and styled it

---
+++ personal-portfolio/src/components/FooterTitle.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './FooterTitle.css';
+
+const FooterTitle = ({ titleText }) => {
+    return <h4 className="footer-column-title">{titleText}</h4>;
+};
+
+export default FooterTitle;
