fe4b27e2975a42494d5d42c5563006327a2b8fdc
salmamoussaa25 <salmamoussaa25@gmail.com>
2025-12-21 10:38:56 +0200

    fix: edited Arabic Langage Toggle button and fixed some styling + animations

--- personal-portfolio/src/components/LanguageAr.jsx
+++ personal-portfolio/src/components/LanguageAr.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './LanguageAr.css'
+import './LanguageAr.css';
 
-const LanguageAr = (props) => {
-    return ( <>
-        <Link to="/">
-            <button className='menu_arabicbutton'>{props.title}</button>
+const LanguageAr = ({ title, to = "/" }) => {
+    return (
+        <Link to={to} className="arabic-link-wrapper">
+            <button className='menu_arabicbutton'>{title}</button>
         </Link>
-    </> );
+    );
 }
  
 export default LanguageAr;
