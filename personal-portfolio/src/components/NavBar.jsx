e4c104bdc78798a2a5164ccb9cb95d0d3c8ad36d
salmamoussaa25 <salmamoussaa25@gmail.com>
2025-12-21 10:42:34 +0200

    fix: added Language toggle to new Nvabar component

--- personal-portfolio/src/components/NavBar.jsx
+++ personal-portfolio/src/components/NavBar.jsx
@@ -1,19 +1,24 @@
 import React from 'react';
 import './NavBar.css';
 import MenuButton from './MenuButton';
+import LanguageAr from './LangAr';
+import nav_logo from '../assetss/nav_logo.png';
+
 
 const NavBar = () => {
     return (
         <nav className="navbar">
-            <div className="nav-logo">
-                sal’s <span>studio</span>
-            </div>
-            <div className="nav-links">
-                <MenuButton title="Home" to="/" />
-                <MenuButton title="About" to="/about" />
-                <MenuButton title="Services" to="/services" />
-                <MenuButton title="Projects" to="/projects" />
-                <MenuButton title="Contact" to="/contact" />
+            <img src={nav_logo} alt="" />
+            <div className="nav-links-container">
+                <div className="nav-main-links">
+                    <MenuButton title="Home" to="/" />
+                    <MenuButton title="About" to="/about" />
+                    <MenuButton title="Services" to="/services" />
+                    <MenuButton title="Projects" to="/projects" />
+                    <MenuButton title="Contact" to="/contact" />
+                </div>
+            
+                <LanguageAr title="العربية" to="/ar" />
             </div>
         </nav>
     );
